640cc57d55f05585b05bb30d5fd7fe3e69ee563b
35192 <35192@DESKTOP-G5AEJBH>
2022-10-29 09:51:12 +0100

    Modify products.

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -8,6 +8,10 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required] //Nome vai ser obrigatório preencher
+
+        //O campo nome não pode conter mais do que 50 caracteres
+        [MaxLength(50, ErrorMessage ="The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
 
@@ -20,11 +24,11 @@ namespace SuperShop.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; } // A data será opcional
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Last Sale")]
